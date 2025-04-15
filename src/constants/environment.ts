26ea1c204ac8741998d26e0d7b685ef68852d2bf
ashertoufeeq <ashertoufeeq@gmail.com>
2025-04-16 04:04:37 +0530

    fix(*): flow done

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_BASE_URL = "http://localhost:9092/"; // Live
-// export const DEFAULT_BASE_URL = "https://api-stage.plem.in/"; // Live
+// export const DEFAULT_BASE_URL = "http://localhost:9092/"; // Live
+export const DEFAULT_BASE_URL = "https://api-stage.plem.in/"; // Live
 export const mockData = false
 export const DEEPLINK_URL = "https://plem.onelink.me";
